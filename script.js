7f996e06a2507b88bf166fd6f42e64c06b21cae9
businesszx05-del <businesszx05@gmail.com>
2026-07-07 11:19:24 +0500

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
+// Password Check
 function checkPassword(){
 
-let password=document.getElementById("password").value;
+let password = document.getElementById("password").value;
 
-if(password==="1234"){
+if(password === "1234"){
 
-window.location.href="cake.html";
+window.location.href = "cake.html";
+
+}else{
+
+document.getElementById("error").innerHTML =
+"❌ Wrong Password";
 
 }
 
-else{
-
-document.getElementById("error").innerHTML="❌ Wrong Password";
-
 }
+
+// Cake Page Next Button
+function nextPage(){
+
+window.location.href = "message.html";
 
 }
